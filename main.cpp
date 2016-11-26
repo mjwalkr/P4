422d1c59b6d4dba1d5e02b200e5046555c630334
mjwalkr <mjwalkr@yahoo.com>
2016-11-26 12:37:07 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+  std::cout << std::setprecision(16); // show 16 digits
+  float f = 3.33333333333333333333333333333333333333f;
+  std::cout << f << std::endl;
+  float b = f;
+  std::cout << b << std::endl;
+  return 0;
+}
