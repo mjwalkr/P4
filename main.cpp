40d172206fa39079a268f370ab4ec8821d19cbf5
mjwalkr <mjwalkr@yahoo.com>
2016-11-26 12:40:56 -0600

    Initial commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
   std::cout << std::setprecision(16); // show 16 digits
-  float f = 3.33333333333333333333333333333333333333f;
+  float f = 3.3333333333333333333333333333333333333f;
   std::cout << f << std::endl;
   float b = f;
   std::cout << b << std::endl;
